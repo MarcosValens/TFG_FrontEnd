f22d063dd6efab45bdfb6ec45755717a923ed57d
unknown <alexis.platino@gmail.com>
2020-05-19 20:37:07 +0200

    Changes will be replaced

--- src/components/Main/Tables/HostsTable/HostsTable.vue
+++ src/components/Main/Tables/HostsTable/HostsTable.vue
@@ -209,7 +209,7 @@ export default {
       ],
       shouldOpenUpdateHostModal: false,
       shouldOpenUpdatePortModal: false,
-      electron: isElectron(),
+      electron: !isElectron(),
       model: {},
       option: [],
       locked: false,
